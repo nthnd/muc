1a5150453e2843335efb09dfac27b512b02d9464
nathan dawit <nathandawit555@gmail.com>
2023-03-14 20:51:28 +0300

    refactor: display extra information
    display total and "others" in output

--- src/display.rs
+++ src/display.rs
@@ -14,12 +14,13 @@ pub fn print(data: CommandMap, args: Args) {
         .map(|(s, (f, o, h))| (f, (s, o, h)))
         .collect();
 
-    let total = tree.len();
+    let total: usize = tree.keys().sum();
     let max = *tree.last_key_value().unwrap().0;
 
-    let limited_tree: Vec<(usize, VeryComplexType)> = tree.into_iter().rev().collect();
+    let reversed_tree: Vec<(usize, VeryComplexType)> = tree.into_iter().rev().collect();
+    let limited_tree = reversed_tree[..(usize::min(args.count, reversed_tree.len()))].to_vec();
 
-    for (freq, elem) in limited_tree[..(usize::min(args.count, limited_tree.len()))].iter() {
+    for (freq, elem) in limited_tree.iter() {
         let (s, _o, h) = elem;
         let mut sub_commands = h.iter().collect::<Vec<(&String, &usize)>>();
         sub_commands.sort_by(|a, b| b.1.cmp(a.1));
@@ -27,16 +28,30 @@ pub fn print(data: CommandMap, args: Args) {
         let sub_commands = if sub_commands.is_empty() {
             None
         } else {
-            Some( 
+            Some(
                 sub_commands[..(usize::min(3, sub_commands.len()))]
-                .iter()
-                .map(|x| x.0.to_owned())
-                .collect()
+                    .iter()
+                    .map(|x| x.0.to_owned())
+                    .collect(),
             )
         };
 
         print_command(s, *freq, max, total, &args, sub_commands);
     }
+
+    let others = total - limited_tree.iter().fold(0, |acc, x| acc + x.0);
+    let other_percentage = (others as f64 / total as f64) * 100.;
+    println!(
+        "{gray}... {} ({:.2}%) others{reset}",
+        others,
+        other_percentage,
+        reset = aecir::style::reset_all(),
+        gray = Color::Fg(ColorName::LightBlack),
+    );
+    println!(
+        "Total: {} commands" ,
+        total
+    );
 }
 
 pub fn print_command(
@@ -47,11 +62,13 @@ pub fn print_command(
     args: &Args,
     sub_commands: Option<Vec<String>>,
 ) {
-    let percentage = invocations as f32 / total as f32;
+    let percentage = (invocations as f32 / total as f32) * 100.0;
     let num_of_bars = ((invocations as f32 / max as f32) * 10.) as usize;
-    let bars = args.bar.fill.repeat(num_of_bars);
-    let empties = args.bar.empty.repeat(10 - num_of_bars);
-    let bar: String = format!("{}{}", bars, empties);
+    let bar: String = format!(
+        "{}{}",
+        args.bar.fill.repeat(num_of_bars),
+        args.bar.empty.repeat(10 - num_of_bars)
+    );
     let pretty_sub_commands = if let Some(sub_commands) = sub_commands {
         let trim_len = sub_commands.len().min(3);
         let mut x = sub_commands[..trim_len].join(", ");
